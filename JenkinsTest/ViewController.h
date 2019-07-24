e2e3d5e3ca296a218d397eeab561e131f15c9e9c
busishenhuo <yuanfangtianguo@126.com>
2019-07-24 12:24:32 +0800

    Git测试

---
+++ JenkinsTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JenkinsTest
+//
+//  Created by Chen on 2019/7/24.
+//  Copyright © 2019 Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JenkinsTest/ViewController.m
@@ -0,0 +1,24 @@
+//
+//  ViewController.m
+//  JenkinsTest
+//
+//  Created by Chen on 2019/7/24.
+//  Copyright © 2019 Chen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    NSLog(@"c横岗");
+}
+
+
+@end
